c3c07711759279abb77140c1652084f5725ccc9e
Karol <kpulawski13@gmail.com>
2023-11-17 14:12:35 +0000

    introduced Character Card component

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
+import CharacterCard from './CharacterCard';
 
-function CardGame() {
+function CardGame({ handleScoreUpdate }) {
   const [data, setData] = useState('');
 
+  // Fetches data with Rick Morty Characters
   useEffect(() => {
     let ignore = false;
     fetch(
-      'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12',
+      'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12', // Ids of characters to fetch
       {}
     )
       .then((response) => response.json())
@@ -17,14 +19,24 @@ function CardGame() {
       });
     return () => (ignore = true);
   }, []);
+
+  function handleCardClick() {
+    handleScoreUpdate();
+  }
   return (
     <div>
       {data ? (
         <div>
           {data.map((character) => (
-            <div key={character.id}>
-              {character.name}, {}
-            </div>
+            // <div key={character.id}>
+            //   {character.name}, {}
+            // </div>
+            <CharacterCard
+              handleCardClick={handleCardClick}
+              key={character.id}
+              name={character.name}
+              url={character.image}
+            />
           ))}
         </div>
       ) : (
