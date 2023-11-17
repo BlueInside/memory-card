b9415f386c9926a6bfe026a419d68794e92753bf
Karol <kpulawski13@gmail.com>
2023-11-17 14:55:35 +0000

    shuffle data on click

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,38 +1,36 @@
 import { useEffect, useState } from 'react';
 import CharacterCard from './CharacterCard';
-
-function CardGame({ handleScoreUpdate }) {
+import { shuffleArray } from '../utils/utils';
+function CardGame({ handleScoreUpdate, handleScoreReset }) {
   const [data, setData] = useState('');
 
   // Fetches data with Rick Morty Characters
   useEffect(() => {
-    let ignore = false;
+    console.log('effect');
     fetch(
       'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12', // Ids of characters to fetch
       {}
     )
       .then((response) => response.json())
       .then((response) => {
-        if (!ignore) {
-          setData(response);
-        }
-      });
-    return () => (ignore = true);
+        setData(response);
+      })
+      .catch((error) => console.log('Error', error));
   }, []);
 
   function handleCardClick() {
     handleScoreUpdate();
+    const shuffledArray = shuffleArray(data);
+    setData(shuffledArray);
   }
   return (
     <div>
       {data ? (
         <div>
           {data.map((character) => (
-            // <div key={character.id}>
-            //   {character.name}, {}
-            // </div>
             <CharacterCard
               handleCardClick={handleCardClick}
+              handleScoreReset={handleScoreReset}
               key={character.id}
               name={character.name}
               url={character.image}
